3a7ac5c5f667afce55e8027a2d5aaf222022817d
LorisDonovan <a7shky@gmail.com>
2021-11-14 21:52:54 +0545

    STL - multiset and set updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-	
+	constexpr int32_t edge = 6;
+	std::vector<std::vector<int32_t>> binaryTree;
+
+	binaryTree.reserve(edge);
 }
--- src/stl/multiset.cpp
+++ src/stl/multiset.cpp
@@ -1,9 +1,9 @@
 /**
- * std::set<Type, Compare>
+ * std::multiset<Type, Compare>
  * 
- * 1. Associative container that contains a "sorted" set of unique objects
- * 2. Implemented using Red-black tree
- * 3. Insertion, removal and search has logarithmic complexity
+ * 1. Associative container that can contain a sorted set of duplicate objects
+ * 2. Implemented as a Red-black tree
+ * 3. Insertion, Removal and Search have logarithmic complexity
  * 4. Storing user defined types in std::set requires a compare function
  * 5. We can also pass the order for sorting
  *    std::set<type, std::greater<>/std::less<>>
@@ -27,45 +27,43 @@
 #include <iostream>
 #include <set>
 #include <string>
-#include <vector>
-#include <functional> // for std::greater and std::less
+#include <functional>
 
 class Book
 {
 public:
 	Book(int32_t year, const std::string& title)
-		: year{year}, title{title} {}
+		: year(year), title(title) {}
 
-	// overloading comparision operators for std::set
 	inline bool operator< (const Book& other) const { return year < other.year; }
 	inline bool operator> (const Book& other) const { return year > other.year; }
 
-	friend std::ostream& operator<< (std::ostream& stream, const Book& book);
-
+	friend std::ostream& operator<< (std::ostream& stream, const Book& book)
+	{
+		stream << book.year << ": " << book.title;
+		return stream;
+	}
+	
 public:
 	int32_t year;
 	std::string title;
 };
 
-std::ostream& operator<< (std::ostream& stream, const Book& book)
-{
-	stream << book.year << ": " << book.title;
-	return stream;
-}
-
 int main()
 {
-	std::set<int32_t> nums = { 1, 2, 3, 4, 5, 1, 2, 3 };
-	for (auto& n : nums)
-		std::cout << n << '\n';
+	std::multiset<int32_t> multiSet = { 1, 2, 3, 4, 5, 1, 3, 5 };
 
-	std::set<Book, std::greater<Book>> books = { 
+	for (auto& m : multiSet)
+		std::cout << m << '\n';
+	std::cout << "------------------\n";
+
+	std::multiset<Book, std::greater<Book>> books = { 
 		{ 1998, "A Clash of Kings" }, 
 		{ 2011, "A Dance with Dragons" }, 
 		{ 1996, "A Game of Thrones" }, 
 		{ 2005, "A Feast for Crows" }, 
 		{ 2000, "A Storm of Swords" }, 
-		{ 2005, "The Book Thief" } // this will be omitted
+		{ 2005, "The Book Thief" } // this won't be omitted unlike std::set
 	};
 
 	books.emplace(Book(2006, "some book"));
@@ -74,7 +72,7 @@ int main()
 	for (auto& b : books)
 		std::cout << b << '\n';
 
-	Book agot(1996, "A Game of Thrones");
-	if (books.find(agot) != books.end())
+	Book AGOT(1996, "A Game of Thrones");
+	if (books.find(AGOT) != books.end())
 		std::cout << "Book found\n";
 }

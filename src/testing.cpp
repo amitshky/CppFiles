86cfa4da13fd237f8f73aa5e87dcba26533fc880
amitshky <a7shky@gmail.com>
2021-12-11 14:31:51 +0545

    empty testing.cpp file

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-#include <bitset>
 
 int main()
 {
-	float num = -1.1f;
-	long bits = *(long*)&num;
-	std::cout << std::bitset<32>(bits) << '\n';
-
-	float num2 = *(float*)&bits;
-	std::cout << num2 << '\n';
 }
